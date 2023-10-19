692b709712c6f718fa682a52db558733d2197e2b
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-10-18 21:45:15 -0300

    update cadastroScript

--- src/main/resources/static/script/cadastroScript.js
+++ src/main/resources/static/script/cadastroScript.js
@@ -1,21 +1,32 @@
 const formulario = document.querySelector("form");
-
 const Iemail = document.querySelector("#inputEmail");
 const isenha = document.querySelector("#inputPassword");
 const isenha2 = document.querySelector("#inputPassword2");
 
+let arrEmails = [];
+
+//só deus sabe pq isso nao ta funcionando
 function cadastrar(){
-    fetch("http://localhost:8080/api/clients",{
-        headers: {
-            'Accept': "application/json",
-            'Content-type' : "application/json"
-        },
-        method: "POST",
-        body: JSON.stringify({
-            email: Iemail.value,
-            password: isenha.value
-    })
-})
+      preencherArrayComEmailsCadastrados();
+      console.log(arrEmails);
+      console.log(Iemail.value)
+      console.log(arrEmails.includes(Iemail.value));
+      if (arrEmails.includes(Iemail.value)) {
+        alert("Email ja cadastrado!");
+      } else {
+      fetch("http://localhost:8080/api/clients",{
+              headers: {
+                  'Accept': "application/json",
+                  'Content-type' : "application/json"
+              },
+              method: "POST",
+              body: JSON.stringify({
+                  email: Iemail.value,
+                  password: isenha.value
+          })
+      })
+        alert("Cadastro concluido");
+      }
 };
 
 function limpar(){
@@ -24,8 +35,27 @@ function limpar(){
     isenha2.value = "";
 }
 
+function preencherArrayComEmailsCadastrados() {
+  const items = fetch("http://localhost:8080/api/clients")
+    .then((data) => {
+      return data.json();
+    })
+    .then((post) => {
+      post.forEach((items) => {
+        arrEmails.push(items.email);
+      });
+    });
+}
+
 formulario.addEventListener("submit", function(event){
     event.preventDefault();
-    cadastrar();
-    limpar();
+    if(isenha.value === isenha2.value){
+        cadastrar();
+        limpar();
+    }
+    else{
+    alert("Senhas não são identicas");
+    }
 });
+
+
