524c99be45c59939893f72154b95b1861dc21e7b
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-10-18 22:06:10 -0300

    update cadastroScript.js

--- src/main/resources/static/script/cadastroScript.js
+++ src/main/resources/static/script/cadastroScript.js
@@ -5,34 +5,38 @@ const isenha2 = document.querySelector("#inputPassword2");
 
 let arrEmails = [];
 
-//só deus sabe pq isso nao ta funcionando
-function cadastrar(){
-      preencherArrayComEmailsCadastrados();
-      console.log(arrEmails);
-      console.log(Iemail.value)
-      console.log(arrEmails.includes(Iemail.value));
-      if (arrEmails.includes(Iemail.value)) {
-        alert("Email ja cadastrado!");
-      } else {
-      fetch("http://localhost:8080/api/clients",{
-              headers: {
-                  'Accept': "application/json",
-                  'Content-type' : "application/json"
-              },
-              method: "POST",
-              body: JSON.stringify({
-                  email: Iemail.value,
-                  password: isenha.value
-          })
-      })
-        alert("Cadastro concluido");
-      }
-};
+//só Deus sabe pq isso nao ta funcionando
+function cadastrar() {
+  preencherArrayComEmailsCadastrados();
+  console.log("Array de email: ", arrEmails);
+  console.log("Email sendo checado: ", Iemail.value);
+  console.log(arrEmails.includes("Tem no array? ", Iemail.value));
+  //esse metodo includes é o capeta!
+  if (arrEmails.includes(Iemail.value)) {
+    alert("Email ja cadastrado!");
+  } else {
+    fetch("http://localhost:8080/api/clients", {
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        email: Iemail.value,
+        password: isenha.value,
+      }),
+    });
+    alert("Cadastro concluido");
+    limpar();
+  }
+}
 
-function limpar(){
-    Iemail.value ="";
-    isenha.value = "";
-    isenha2.value = "";
+function temNoArray() {}
+
+function limpar() {
+  Iemail.value = "";
+  isenha.value = "";
+  isenha2.value = "";
 }
 
 function preencherArrayComEmailsCadastrados() {
@@ -47,15 +51,11 @@ function preencherArrayComEmailsCadastrados() {
     });
 }
 
-formulario.addEventListener("submit", function(event){
-    event.preventDefault();
-    if(isenha.value === isenha2.value){
-        cadastrar();
-        limpar();
-    }
-    else{
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (isenha.value === isenha2.value) {
+    cadastrar();
+  } else {
     alert("Senhas não são identicas");
-    }
+  }
 });
-
-
