6682914b04835c5d68c129f3843df4c8e9cedbfc
Victor Mesquitta <mvictormesquita@gmail.com>
2023-10-28 12:43:02 -0300

    Consertando o problema do includes no cadastro

--- src/main/resources/static/script/cadastroScript.js
+++ src/main/resources/static/script/cadastroScript.js
@@ -5,41 +5,48 @@ const isenha2 = document.querySelector("#inputPassword2");
 
 let arrEmails = [];
 
-//só Deus sabe pq isso nao ta funcionando
 function cadastrar() {
-  console.log("Array de email: ", arrEmails);
-  console.log("Email sendo checado: ", Iemail.value);
-  console.log(arrEmails.includes("Tem no array? ", Iemail.value));
-  //esse metodo includes é o capeta!
-  if (arrEmails.includes(Iemail.value)) {
-    alert("Email ja cadastrado!");
-  } else {
-    fetch("http://localhost:8080/api/clients", {
-      headers: {
-        Accept: "application/json",
-        "Content-type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({
-        email: Iemail.value,
+  preencherArrayComEmailsCadastrados().then(() => {
+    console.log("Array de emails: ", arrEmails);
+    console.log("Email sendo verificado: ", Iemail.value + " " + typeof Iemail.value);
+    console.log("Está no array? ", arrEmails.includes(Iemail.value));
 
-        password: isenha.value,
-      }),
-    });
-    alert("Cadastro concluido");
-    limpar();
-  }
+    if (arrEmails.includes(Iemail.value)) {
+      alert("Email já cadastrado!");
+    } else {
+      fetch("http://localhost:8080/api/clients", {
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          email: Iemail.value,
+          password: isenha.value,
+        }),
+      })
+        .then(() => {
+          alert("Cadastro concluído");
+          limpar();
+        })
+        .catch((error) => {
+          console.error("Erro ao cadastrar: ", error);
+        });
+    }
+  });
 }
 
-function temNoArray() {
-  let i = 0;
-  while (i < arrEmails.length) {
-    if (arrEmails[i] == Iemail.value) {
-      return true;
-    }
-    i++;
-  }
-  return false;
+function preencherArrayComEmailsCadastrados() {
+  return fetch("http://localhost:8080/api/clients")
+    .then((data) => {
+      return data.json();
+    })
+    .then((post) => {
+      arrEmails = post.map((item) => item.email);
+    })
+    .catch((error) => {
+      console.error("Erro ao obter emails cadastrados: ", error);
+    });
 }
 
 function limpar() {
@@ -48,25 +55,11 @@ function limpar() {
   isenha2.value = "";
 }
 
-function preencherArrayComEmailsCadastrados() {
-  const items = fetch("http://localhost:8080/api/clients")
-    .then((data) => {
-      return data.json();
-    })
-    .then((post) => {
-      post.forEach((items) => {
-        arrEmails.push(items.email);
-      });
-    });
-}
-
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
   if (isenha.value === isenha2.value) {
     cadastrar();
   } else {
-    alert("Senhas não são identicas");
+    alert("As senhas não são idênticas");
   }
 });
-
-preencherArrayComEmailsCadastrados();
