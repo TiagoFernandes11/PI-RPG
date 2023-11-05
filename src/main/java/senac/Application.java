f4d9d85ed6d7fd2050c1dff54c1a7880b24dbde4
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-05 19:46:36 -0300

    Update Application.java

--- src/main/java/senac/Application.java
+++ src/main/java/senac/Application.java
@@ -3,6 +3,7 @@ package senac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @CrossOrigin("*")
-@SpringBootApplication(exclude = AutoConfiguration.class)
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
