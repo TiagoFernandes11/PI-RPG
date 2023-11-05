dbfc65721e9a4c2ac705aa017ff0a86a4064e33b
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-05 19:44:00 -0300

    Update PericiasRepository.java

--- src/main/java/senac/domain/repository/PericiasRepository.java
+++ src/main/java/senac/domain/repository/PericiasRepository.java
@@ -2,7 +2,8 @@ package senac.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import senac.domain.model.Pericias;
 
 @Repository
-public interface PericiasRepository extends JpaRepository<Pericia, Integer> {
+public interface PericiasRepository extends JpaRepository<Pericias, Integer> {
 }
