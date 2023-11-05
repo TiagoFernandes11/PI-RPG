b8fcc34c0193d507960cb04f572aba614fe5cacf
Victor Mesquitta <mvictormesquita@gmail.com>
2023-11-05 19:48:42 -0300

    campanhas

---
+++ src/main/java/senac/domain/repository/CampanhaRepository.java
@@ -0,0 +1,10 @@
+package senac.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import senac.domain.model.Campanha;
+import senac.domain.model.Participante;
+
+@Repository
+public interface CampanhaRepository extends JpaRepository<Campanha, Integer>{
+}
